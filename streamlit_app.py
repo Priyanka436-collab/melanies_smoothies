d86eacc29fee46b02dfd2640be27113898ded345
Priyanka436-collab <priyankasherikar8@gmail.com>
2025-03-20 17:50:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,9 @@ if ingredients_list:
 
 
 
-
+import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 
